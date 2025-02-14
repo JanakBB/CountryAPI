05bfea93db4f4a992d0d6d6e924e39554040dfc8
Janak Bahadur Bohara <janakcustomx@gmail.com>
2025-02-14 08:16:51 +0545

    add window size

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,22 +2,22 @@ import SearchBar from "./SearchBar";
 import "../App.css";
 import SearchSelect from "./SelectMenu";
 import CountriesList from "./CountriesList";
-import {useState } from "react";
+import { useState } from "react";
 import useWindowSize from "../hooks/useWindowSize";
 import { useTheme } from "../hooks/useTheme";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [isDark] = useTheme()
+  const [isDark] = useTheme();
   const windowSize = useWindowSize();
 
   return (
     <main className={`main-container ${isDark ? "dark" : ""}`}>
       <div className="search-filter-container">
         <SearchBar setQuery={setQuery} />
-        <SearchSelect />
+        <SearchSelect setQuery={setQuery} />
       </div>
-      <h1 style={{ textAlign: "center" }}>
+      <h1 style={{ textAlign: "center" }}>Window Size: 
         {windowSize.width} x {windowSize.height}
       </h1>
       {<CountriesList query={query} />}
